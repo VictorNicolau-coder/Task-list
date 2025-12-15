8153060018c5adb01acc544c1f1d757006a5783c
VictorNicolau-coder <victoranderson1575@gmail.com>
2025-12-15 13:03:17 -0300

    vercel json added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const app = require('./app')
-const connect = require('./models/connection')
+const app = require('./src/app')
+const connect = require('./src/models/connection')
 require('dotenv').config()
 
 connect()
